a167d62e88be8173b5d45001851de13f97ca3d4a
João Álvaro <joaosilvaabc1@gmail.com>
2026-03-30 17:08:31 -0300

    feat: cors para permitir requisições front-end

--- setup/settings.py
+++ setup/settings.py
@@ -34,6 +34,10 @@ ALLOWED_HOSTS = []
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173"
+]
 
 # Application definition
 
@@ -45,10 +49,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'apps.projetos', # Gerenciamento de Projetos
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
