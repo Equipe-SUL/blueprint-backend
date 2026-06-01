225dec6ccd58d3ff8691336d804d9a7c14d66bff
João Álvaro <joaosilvaabc1@gmail.com>
2026-05-31 21:00:19 -0300

    chore: move dev scripts to scripts/, untrack chroma_db sqlite, update readme

--- apps/projetos/urls.py
+++ apps/projetos/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import UploadArquivoView, ProjetosViewSet, server_status, ItemProjetoView, TesteUploadPlanilhaView, RetomarPipelineView, ProcessarArquivoView, ServirMemorialPDFView, GerarOrcamentoView, ServirOrcamentoPDFView, ExportarMateriaisView, DashboardStatsView
+from .views import UploadArquivoView, ProjetosViewSet, server_status, ItemProjetoView, TesteUploadPlanilhaView, ProcessarArquivoView, ServirMemorialPDFView, GerarOrcamentoView, ServirOrcamentoPDFView, ExportarMateriaisView, DashboardStatsView
 from django.conf import settings
 from django.conf.urls.static import static 
 from rest_framework import routers
@@ -14,7 +14,6 @@ urlpatterns = [
     path('projetos/<int:projeto_id>/upload/', UploadArquivoView.as_view(), name='upload_arquivo'),
     path('projetos/<int:projeto_id>/itens/', ItemProjetoView.as_view(), name='itens_projeto'),
     path('projetos/<int:projeto_id>/itens/<int:item_id>/', ItemProjetoView.as_view(), name='item_projeto_detail'),
-    path('projetos/<int:projeto_id>/retomar/', RetomarPipelineView.as_view(), name='retomar_pipeline'),
     path('projetos/<int:projeto_id>/upload/<int:arquivo_id>/', UploadArquivoView.as_view(), name='delete_arquivo_upload'),
     path('projetos/<int:projeto_id>/processar/<int:arquivo_id>/', ProcessarArquivoView.as_view(), name='processar_arquivo'),
     path('projetos/<int:projeto_id>/memorial/<int:memorial_id>/pdf/', ServirMemorialPDFView.as_view(), name='servir_memorial_pdf'),
